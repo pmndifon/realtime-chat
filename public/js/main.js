26cd412f228a272fc730570401386d548271912f
Patrick Ndifon <pmndifon@gmail.com>
2020-04-07 14:28:42 +0100

    Send chat message from server to frontend

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,35 @@
+const chatForm = document.getElementById('chat-form');
+
 const socket = io();
 
+// Message from server
 socket.on('message', message => {
     console.log(message);
+    outputMessage(message);
 });
+
+// Message Submit
+chatForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // Get message text
+    const msg = e.target.elements.msg.value;
+
+    // Emit message toserver
+    socket.emit('chatMessage', msg);
+
+});
+
+// Output message to DOM
+function outputMessage(message) {
+    const div = document.createElement('div');
+    div.classList.add('message');
+    div.innerHTML = `
+        <p class="meta">Mary <span>9:15pm</span></p>
+        <p class="text">
+            ${message}
+        </p>
+    `;
+    
+    document.querySelector('.chat-messages').appendChild(div);
+}
